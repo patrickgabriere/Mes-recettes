e80a47c9b087d08d09d3a4659378d7448d5a82e6
patrickgabriere <patrick.gabriere@gmail.com>
2026-05-30 19:51:42 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Le Grimoire des Parents",
+  "short_name": "Grimoire",
+  "description": "L'assistant de cuisine de la famille",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#fdf6ec",
+  "theme_color": "#3d2b1f",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/192/3227/3227821.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3227/3227821.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
